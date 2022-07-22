ca77f6352fdacf9011979e7f486396ba7d53f3db
Adil Thalho <adilthalho@hotmail.com>
2022-07-22 00:22:13 -0400

    Adding new project.
    Change-Id: Idee7ca3ec3bf905ff80e01bc44431a7f11002994
    Signed-off-by: Adil Thalho <adilthalho@hotmail.com>

---
+++ Practice/src/com/main/Main.java
@@ -0,0 +1,16 @@
+package com.main;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to main method()");
+		
+		
+	}
+
+}
