b1173e53d78a40cb1c861acd0c4ad62afcebe634
Adil Thalho <adilthalho@hotmail.com>
2022-07-22 13:32:44 -0400

    message change

--- Practice/src/com/main/Main.java
+++ Practice/src/com/main/Main.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to main method()");
+		System.out.println("Welcome to main method().");
 		
 		
 	}
